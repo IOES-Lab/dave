8760b4c22ff5a482b8289616561d662f37dd310d
Woen-Sug Choi <woensug.choi@gmail.com>
2024-08-19 19:10:52 +0900

    add demo launch with ros_gz_bridge

--- gazebo/dave_gz_multibeam_sonar/README.md
+++ gazebo/dave_gz_multibeam_sonar/README.md
@@ -4,7 +4,14 @@
 - The system plugin that will also be defined in `world` file is at `multibeam_sonar_system`
 - We need both the sensor and the system plugin to be able to use the sensor in Gazebo
 
-- To test
+- Launch example with `dave_sensor.launch.py`:
+
   ```
-  ros2 launch dave_demos dave_sensor.launch.py namespace:=blueview_p900 world_name:=dave_multibeam_sonar paused:=false
+  ros2 launch dave_demos dave_sensor.launch.py namespace:=blueview_p900 world_name:=dave_multibeam_sonar paused:=false x:=4 z:=0.5 yaw:=3.14
+  ```
+
+- Launch example with `ros_gz_bridge` to translate gazebo msg to ROS msg:
+
+  ```
+  ros2 launch dave_gz_multibeam_sonar multibeam_sonar_demo.launch.py
   ```
---
+++ gazebo/dave_gz_multibeam_sonar/launch/multibeam_sonar_demo.launch.py
@@ -0,0 +1,72 @@
+# Copyright 2019 Open Source Robotics Foundation, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from ament_index_python.packages import get_package_share_directory
+
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.conditions import IfCondition
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch.substitutions import LaunchConfiguration
+
+from launch_ros.actions import Node
+
+
+def generate_launch_description():
+    pkg_dave_demos = get_package_share_directory('dave_demos')
+    multibeam_sonar_sim = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_dave_demos, 'launch', 'dave_sensor.launch.py')),
+        launch_arguments={
+            'namespace': 'blueview_p900',
+            'world_name': 'dave_multibeam_sonar',
+            'paused': 'false',
+            'x': '4',
+            'z': '0.5',
+            'yaw': '3.14'
+        }.items(),
+    )
+
+    # RViz
+    pkg_dave_gz_multibeam_sonar = get_package_share_directory('dave_gz_multibeam_sonar')
+    rviz = Node(
+        package='rviz2',
+        executable='rviz2',
+        arguments=['-d', os.path.join(pkg_dave_gz_multibeam_sonar, 'rviz', 'multibeam_sonar.rviz')],
+        condition=IfCondition(LaunchConfiguration('rviz'))
+    )
+
+    # Bridge
+    bridge = Node(
+        package='ros_gz_bridge',
+        executable='parameter_bridge',
+        arguments=[
+            '/sensor/camera@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/sensor/camera_info@sensor_msgs/msg/CameraInfo@gz.msgs.CameraInfo',
+            '/sensor/depth_camera@sensor_msgs/msg/Image@gz.msgs.Image',
+            '/sensor/multibeam_sonar/point_cloud@sensor_msgs/msg/PointCloud2@gz.msgs.PointCloudPacked'
+        ],
+        output='screen',
+    )
+
+    return LaunchDescription([
+        multibeam_sonar_sim,
+        DeclareLaunchArgument('rviz', default_value='true',
+                              description='Open RViz.'),
+        bridge,
+        rviz,
+    ])
